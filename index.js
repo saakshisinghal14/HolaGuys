bdda9cb9a63050412c01ed55698d1d1cd511feb4
saa <93211276+saakshisinghal14@users.noreply.github.com>
2021-11-02 21:12:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//node server which will handle socket io connection
+const io = require(`socket.io`)(8000,{
+    cors:{
+        origin:'*',
+    }
+});
+const users={};
+
+io.on('connection',socket=>{
+    socket.on('new-user-joined',name=>{
+        console.log("new user",name);
+ users[socket.id]=name;
+ socket.broadcast.emit('user-joined',name);
+    });
+    socket.on('send',message=>{
+        socket.broadcast.emit('received',{message:message,name:users[socket.id]})
+    });
+
+    socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',users[socket.id])
+        delete users[socket.id];
+    });
+});
+
