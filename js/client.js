11e9b868bf99113b0bcaa6cd33fb43872582b288
saa <93211276+saakshisinghal14@users.noreply.github.com>
2021-11-03 13:43:02 +0530

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket=io('https://saakshisinghal14.github.io/HolaGuys/');
+const socket=io('http://localhost:8000');
 const form= document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
 const messageContainer=document.querySelector(".container");
